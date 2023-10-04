2f52beee1a2f85d8f955c56bf9f7579820279873
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2023-10-04 04:41:38 -0300

    Update and rename core.clj to mais-sobre-mapas/core.clj

--- mais-sobre-mapas/core.clj
+++ mais-sobre-mapas/core.clj
@@ -13,17 +13,17 @@
    :arroz {:quantidade 1 :preco 4}
    :feijao {:quantidade 2 :preco 10}})
 
-; cria um novo map (imutável) e adiciona/substitui elementos, no conj é passado 2 maps e no assoc é passado 1 map e 1 key (chave-valor)
-(conj (compras) {:alface {:quantidade 2 :preco 5}}) ; pop e peek não funcionam com map
-(assoc (compras) :alface {:quantidade 2 :preco 8})
+; cria um novo map (imutável) e adiciona/substitui elementos, pop e peek não funcionam com map
+(conj (compras) {:alface {:quantidade 2 :preco 5}}) ; no conj é passado 2 maps
+(assoc (compras) :alface {:quantidade 2 :preco 8})  ; no assoc é passado 1 map e 1 key (chave-valor)
 
 ; remove elemento(s) do map
-(dissoc (compras) :tomate :arroz)
+(dissoc (compras) :tomate :arroz) ; no dissoc é passado 1 map e 1 ou mais keys
 
-; função update recebe um map, a chave que queremos atualizar e uma função, só funciona para um map
+; função update recebe um map, a chave que queremos atualizar, e uma função (só funciona para um map)
 (update{:nome "Daniel Lisboa" :idade 21} :idade inc)
 (inc 39) ; 40
 
 ; usamos a função update-in para atualizar map-map ou map-map-map...
-(update-in(compras) [:tomate :preco] inc)
+(update-in(compras) [:tomate :preco] inc) ; 1 + preço do tomate
 (update-in(compras) [:tomate :preco] * 2) ; 2 * preço do tomate
