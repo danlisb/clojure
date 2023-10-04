4c768e8b5500757ebdda61de3b0c91c70f7f368d
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2023-10-04 06:52:10 -0300

    Add files via upload

---
+++ core.clj
@@ -0,0 +1,50 @@
+(ns disciplinas.core
+  (:require [clojure.string :refer [upper-case join]]))
+
+; Desafio: precisamos criar uma função que dada um vetor de disciplinas e discentes, deve:
+; - Filtrar a lista para exibir disciplinas restantes (que sejam do semestre atual ou superior)
+; - Transformar nome da disciplina para maiúsculo e descartar demais informações
+; - Criar uma String concatenando o nome de todas as disciplinas filtradas
+; Exemplo de entrada:
+; [{:nome "Estrutura de Dados" :semestre 2}
+; {:nome "Algoritmos" :semestre 1}
+; {:nome "Inteligência Artificial" :semestre 3}] 2
+; Saída esperada: "ESTRUTURA DE DADOS, INTELIGÊNCIA ARTIFICIAL"
+
+(defn disciplinas
+  []
+  [{:nome "Estrutura de Dados" :semestre 2}
+   {:nome "Algoritmos" :semestre 1}
+   {:nome "Inteligência Artificial" :semestre 3}])
+
+(disciplinas)
+
+(defn nomes-disciplinas-restantes
+  [disciplinas semestre-atual]
+  (join ", "(map upper-case (map :nome (filter #(>= (:semestre %) semestre-atual) disciplinas)))))
+
+; reescrevendo a função anterior com threading last
+(defn nomes-disciplinas-restantes
+  [disciplinas semestre-atual]
+  (->> disciplinas
+       (filter #(>= (:semestre %) semestre-atual))
+       (map :nome)
+       (map upper-case)
+       (join ", ")))
+
+(nomes-disciplinas-restantes (disciplinas) 2)
+
+(defn transforma
+  [pessoa]
+  (update (assoc pessoa :cor-cabelo :cinza) :idade inc))
+
+(transforma {:nome "Socrates" :idade 39})
+
+; refazendo a função anterior com threading first
+(defn transforma*
+  [pessoa]
+  (-> pessoa
+      (assoc :cor-cabelo :cinza)
+      (update :idade inc)))
+
+(transforma* {:nome "Socrates" :idade 39})
