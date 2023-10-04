4b0fc497aca09d7e0013ed998c15fb14d6d2c706
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2023-10-04 07:49:29 -0300

    Create logic.clj

---
+++ calculo-imc/logic.clj
@@ -0,0 +1,18 @@
+(ns calculo-imc.logic)
+
+; dado um peso e altura de uma pessoa, retorne seu imc (índice de massa corporal)
+; imc = (peso / (altura^2))
+; abaixo de 18,5: peso baixo
+; entre 18,5 e 24,9: peso ideal
+; entre 24,9: acima do peso ideal
+
+(defn valor-imc [peso altura]
+  (/ peso (* altura altura)))
+
+(defn indicador-imc [peso altura]
+  (let [resultado-calculo-imc (valor-imc peso altura)]
+    (if (< resultado-calculo-imc 18.5)
+      :peso-baixo
+      (if (< resultado-calculo-imc 24.9)
+        :peso-ideal
+        :peso-acima))))
